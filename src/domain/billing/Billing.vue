d71f4596fde7c2a03daf69623dd96290683a197f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-11 17:13:30 +0300

    restyles ui

--- src/domain/billing/Billing.vue
+++ src/domain/billing/Billing.vue
@@ -31,7 +31,7 @@ function select(tab: string) {
         <label class="text-lg mx-1">Finances</label>
       </div>
       <div class="">
-        <span class=" mt-5 mr-3 text-black-800 rounded-md px-1 py-0.5 text-md">{{totalBalance.current.toLocaleString()}}/=</span>
+        <span class=" mt-5 mr-3 text-black-800 font-semibold rounded-md px-1 py-0.5 text-md">{{totalBalance.current.toLocaleString()}}/=</span>
       </div>
     </div>
     <div class="flex pt-5">
