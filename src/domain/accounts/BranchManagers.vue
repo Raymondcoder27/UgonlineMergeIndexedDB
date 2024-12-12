152e1a8715b21b3713b626775b089913083d5ee6
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-12 18:26:07 +0300

    restyles dashboard

--- src/domain/accounts/BranchManagers.vue
+++ src/domain/accounts/BranchManagers.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import AppModal from "@/components/AppModal.vue";
 import { useAccounts } from "@/domain/accounts/stores";
-import { onMounted, type Ref, ref, watch, reactive } from "vue";
+import { onMounted, type Ref, ref, watch, reactive, computed } from "vue";
 import CreateAccount from "@/domain/accounts/components/CreateAccount.vue";
 import moment from "moment";
 import type { IGoFilter } from "@/types"
