b44c9cb8d67173267b1fe168835500e16160242f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-12 16:48:17 +0300

    restyles dashboard ui

--- src/domain/branches/AppBranches.vue
+++ src/domain/branches/AppBranches.vue
@@ -386,9 +386,9 @@ onMounted(() => {
       <div class="py-1">
         <span class="px-2 py-1 bg-primary rounded text-white">{{ page }}</span>
         <label class="mx-1 text-gray-400">/</label>
-        <span class="px-2 py-1 bg-primary-50 rounded text-primary-600">
+        <!-- <span class="px-2 py-1 bg-primary-50 rounded text-primary-600">
           {{ totalPages }}
-        </span>
+        </span> -->
       </div>
 
       <!-- Next Button -->
