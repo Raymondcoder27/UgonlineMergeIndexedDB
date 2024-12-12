361164e8360251200e0739151b456d310a152a94
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-12 12:57:47 +0300

    restyles dashboard

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Logo from "@/assets/images/banner.png"
+import Logo from "@/assets/images/banner.png";
 import { useStorage } from "@vueuse/core";
 import { computed, unref, ref } from "vue";
 import type { ComputedRef } from "vue";
@@ -27,6 +27,7 @@ const sideMenuOpen = ref(useStorage("sms", true));
                 <b class="bg-yellow-500 p-1">On</b>
                 <b class="rounded-r bg-primary-700 p-1">line</b>
               </span>
+              </div>
               <div
         :class="{ 'w-full': menuOpen, 'group w-auto hover:w-full': !menuOpen }"
         class="relative bg-white flex items-center justify-between z-10 pt-2"
@@ -36,7 +37,6 @@ const sideMenuOpen = ref(useStorage("sms", true));
     <p class="font-light text-gray-500 text-xs">mwebraymond13@gmail.com</p>
     <!-- <p class="font-semibold text-gray-500 text-xs">BALANCE: 675,000/=</p> -->
     </div>
-              </div>
             </div>
           </div>
 
