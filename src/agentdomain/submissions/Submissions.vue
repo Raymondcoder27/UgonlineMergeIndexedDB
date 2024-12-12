c14be2834955fd18481330b2867c7f3d629c5985
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-12 19:27:32 +0300

    restyles dashboard ui

--- src/agentdomain/submissions/Submissions.vue
+++ src/agentdomain/submissions/Submissions.vue
@@ -217,7 +217,7 @@ watch(
   <!-- <div class="flex flex-col min-h-[85vh]"> -->
     <div class="">
     <!-- Header -->
-    <div class="max-w-7xl mx-auto bg-white p-2 flex flex-col min-h-[85vh]">
+    <div class="max-w-7xl mx-auto bg-white p-2 flex flex-col min-h-[85vh] justify-center items-center">
       <div class="flex space-x-2 my-1 pt-1 pb-3">
       <div class="flex-grow">
         <div class="grid grid-cols-6 gap-2 bg-gray-10 border border-gray-200 rounded px-2 py-3">
