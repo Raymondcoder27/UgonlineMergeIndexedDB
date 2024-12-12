4d8fe2f35df71dbacb2e81f3e63604ab32d84907
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-12 19:03:57 +0300

    restyles dashboard

--- src/agentdomain/submissions/Submissions.vue
+++ src/agentdomain/submissions/Submissions.vue
@@ -401,7 +401,7 @@ watch(
                 </div>
                 <div class="" v-if="transaction.status === 'submitted'">
                   <span
-                    class="bg-green-500 rounded-md font-semibold text-white px-1 py-0.5"
+                    class="bg-green-500 rounded-md font-semibold text-white px-1 py-1"
                     @click="confirm(transaction)"
                   >
                   <i class="fa fa-eye"></i>
