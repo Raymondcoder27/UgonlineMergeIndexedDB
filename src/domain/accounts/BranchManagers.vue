89e3ad89e070b3ca79b77e4583732e6d55b57b4a
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-12 18:26:23 +0300

    restyles dashboard ui

--- src/domain/accounts/BranchManagers.vue
+++ src/domain/accounts/BranchManagers.vue
@@ -199,7 +199,7 @@ onMounted(() => {
           <!-- <tr :class="account.blockedAt ? 'body-tr-blocked' : 'body-tr'"
             v-for="(account, idx) in store.managerAccounts" :key="idx"> -->
             <tr :class="account.blockedAt ? 'body-tr-blocked' : 'body-tr'"
-            v-for="(account, idx) in paginatedManagersAccounts" :key="idx"></tr>
+            v-for="(account, idx) in paginatedManagersAccounts" :key="idx">
 <!--            <td width="10px">{{ idx + 1 }}.</td>-->
             <td>
               <label class="font-bold py-1">
