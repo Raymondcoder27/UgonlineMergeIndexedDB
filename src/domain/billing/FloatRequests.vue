f6b2ba68955e2690e0fadf557cb2931af38e195c
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-13 13:05:19 +0300

    restyles dashboard ui

--- src/domain/billing/FloatRequests.vue
+++ src/domain/billing/FloatRequests.vue
@@ -341,9 +341,9 @@ onMounted(() => {
           <button
             class="px-1 py-0.5 text-red-600 rounded-md hover:bg-red-700 hover:text-white focus:outline-none focus:ring focus:ring-red-300 disabled:opacity-50 disabled:cursor-not-allowed"
             :class="{
-              'opacity-50 cursor-not-allowed': floatLedgers.length < limit,
+              'opacity-50 cursor-not-allowed': floatRequests.length < limit,
             }"
-            :disabled="floatLedgers.length < limit"
+            :disabled="floatRequests.length < limit"
             @click="next"
           >
             <i class="fa-solid fa-arrow-right"></i>
