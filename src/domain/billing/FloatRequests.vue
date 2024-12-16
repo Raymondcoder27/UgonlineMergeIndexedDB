9fc80d2600e317a5f9a473573bc689497371bee4
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-16 15:27:28 +0300

    restyles dashboard

--- src/domain/billing/FloatRequests.vue
+++ src/domain/billing/FloatRequests.vue
@@ -225,7 +225,7 @@ onMounted(() => {
       <table class="table">
         <thead>
           <tr class="header-tr">
-            <!-- <th class="text-left">#</th> -->
+            <th class="text-left">#</th>
             <th class="text-left">Date</th>
             <!-- <th class="text-left">Name</th> -->
             <th class="text-left">Branch</th>
@@ -250,7 +250,7 @@ onMounted(() => {
             v-for="(request, id) in paginatedFloatRequests"
             :key="id"
           >
-            <!-- <td>{{ idx + 1 }}</td> -->
+            <td>{{ idx + 1 }}</td>
             <!-- <td class="text-left">{{  convertDateTime(request.date) }}</td> -->
             <td class="text-left">{{ request.requestDate }}</td>
             <!-- convertDateTime(request.createdAt) -->
