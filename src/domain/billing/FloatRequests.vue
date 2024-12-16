4d8b8d913b0ecbec05f60ae3d6be3c72c08838ff
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-16 11:01:28 +0300

    restyles dashboard

--- src/domain/billing/FloatRequests.vue
+++ src/domain/billing/FloatRequests.vue
@@ -269,7 +269,7 @@ onMounted(() => {
                 > -->
                 <span
                   class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-green-700 bg-green-100 border border-green-300 hover:text-green-700 hover:bg-green-200"
-                  @click="approveFloatRequest(request.branch)"
+                  @click="approveFloatRequest(request.id)"
                   >
                   <i class="fa-solid fa-check"></i>
                   Approved</span
