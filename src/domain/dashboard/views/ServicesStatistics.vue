f70b66d1cbc3eb1c685f3fa48aca63f7319e6648
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-17 15:56:14 +0300

    restyles dashboard ui

--- src/domain/dashboard/views/ServicesStatistics.vue
+++ src/domain/dashboard/views/ServicesStatistics.vue
@@ -241,7 +241,7 @@ function select(tab:string){
         </div>
         <div :class="(activeTab == 'services') ? 'tab-active' : 'tab'" @click="select('services')">
           <div class="w-full py-1">
-            <label class="p-3">Line Graph showing Service Applications</label>
+            <label class="p-3">Service Applications Line Graph</label>
             <i class="fa-solid fa-chart-area float-right px-2 py-1" v-if="activeTab == 'services'"></i>
           </div>
         </div>
