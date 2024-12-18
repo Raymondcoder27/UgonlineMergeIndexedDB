1a53fe7d8b6f2c2380248ff89b3dc951859436ec
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-18 10:07:30 +0300

    restyles dashboard ui

--- src/domain/finances/FloatAllocation.vue
+++ src/domain/finances/FloatAllocation.vue
@@ -2,7 +2,7 @@
 import AppModal from "@/components/AppModal.vue";
 import { useBilling } from "@/domain/finances/stores";
 import { onMounted, type Ref, ref, reactive, watch, computed } from "vue";
-import AllocateFloat from "@/domain/billing/components/AllocateFloat.vue";
+import AllocateFloat from "@/domain/finances/components/AllocateFloat.vue";
 import { useDebounceFn } from "@vueuse/core";
 import type { IGoFilter } from "@/types";
 import moment from "moment";
