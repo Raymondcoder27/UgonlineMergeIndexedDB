3c2e1a006c139b7100b623afab660547c4495831
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-18 15:12:40 +0300

    restyles dashboard ui

--- src/domain/dashboard/views/RevenueStatistics.vue
+++ src/domain/dashboard/views/RevenueStatistics.vue
@@ -66,15 +66,11 @@ function labelExtractor(data:Statistic[]){
         <div class="w-full">
           <div class="flex my-2">
             <div class="w-2/12 count">
-              <p class="text-xl font-bold ml-2">
+              <p class="text-xl font-bold py-2">
           {{ totalBalance.currentBalance.toLocaleString() }}/=
         </p>
               <!-- <p class="text-xl font-bold py-2">{{Number(6341883900).toLocaleString()}}</p> -->
-              <p class="text-xl font-bold py-2">Balance</p>
-            </div>
-            <div class="w-2/12 count">
-              <p class="text-xl font-bold py-2">{{Number(6274328300).toLocaleString()}}</p>
-              <p class="text-xs">Float Requests</p>
+              <p class="text-xs">Balance</p>
             </div>
             <div class="w-2/12 count">
               <p class="text-xl font-bold py-2">{{Number(189334600).toLocaleString()}}</p>
@@ -83,6 +79,18 @@ function labelExtractor(data:Statistic[]){
             <div class="w-2/12 count">
               <p class="text-xl font-bold py-2">{{Number(67555600).toLocaleString()}}</p>
               <p class="text-xs">Failed Transactions</p>
+            </div>
+            <div class="w-2/12 count">
+              <p class="text-xl font-bold py-2">{{Number(6274328300).toLocaleString()}}</p>
+              <p class="text-xs">Total Float Requests</p>
+            </div>
+             <div class="w-2/12 count">
+              <p class="text-xl font-bold py-2">{{Number(5433328300).toLocaleString()}}</p>
+              <p class="text-xs">Approved Float Requests</p>
+            </div>
+            <div class="w-2/12 count">
+              <p class="text-xl font-bold py-2">{{Number(908555600).toLocaleString()}}</p>
+              <p class="text-xs">Rejected Float Requests</p>
             </div>
             <!-- <div class="w-2/12 count">
               <p class="text-xl font-bold py-2">{{Number(5433328300).toLocaleString()}}</p>
