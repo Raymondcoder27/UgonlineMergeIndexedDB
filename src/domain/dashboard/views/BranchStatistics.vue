8a791f8ae09f69930125700d015579325becb83e
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-18 15:35:25 +0300

    restyles dashboard

--- src/domain/dashboard/views/BranchStatistics.vue
+++ src/domain/dashboard/views/BranchStatistics.vue
@@ -71,7 +71,7 @@ function labelExtractor(data:Statistic[]){
                          :labels="labelExtractor(providerOnboarding[0].data)"/>
             </div>
             <div class="w-6/12">
-              <PieChart title="Distribution of Till Operators by Gender" :data="[78, 22]" :labels="['Government','Private']"/>
+              <PieChart title="Distribution of Till Operators by Gender" :data="[78, 22]" :labels="['Male','Female']"/>
             </div>
           </div>
         </div>
