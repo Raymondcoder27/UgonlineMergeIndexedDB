f4d53a25f385f089a5d1c87af62d7589701a35c8
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-18 15:39:12 +0300

    restyles dashboard

--- src/domain/dashboard/views/ProviderStatisticsBarChart.vue
+++ src/domain/dashboard/views/ProviderStatisticsBarChart.vue
@@ -224,17 +224,18 @@ function labelExtractor(data: Statistic[]) {
         <div class="w-full">
           <div class="flex my-2">
             <div class="w-2/12 count">
-              <p class="text-xl font-bold py-2">60</p>
+              <!-- <p class="text-xl font-bold py-2">60</p> -->
+              <p class="text-xl font-bold py-2">8</p>
               <p class="text-xs">Total Services</p>
             </div>
             <div class="w-2/12 count">
               <!-- <p class="text-xl font-bold py-2">50</p> -->
-              <p class="text-xl font-bold py-2">8</p>
+              <p class="text-xl font-bold py-2">7</p>
               <p class="text-xs">Active</p>
             </div>
             <div class="w-2/12 count">
               <!-- <p class="text-xl font-bold py-2">10</p> -->
-              <p class="text-xl font-bold py-2">2</p>
+              <p class="text-xl font-bold py-2">1</p>
               <p class="text-xs">Inactive</p>
             </div>
           </div>
