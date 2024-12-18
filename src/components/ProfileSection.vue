0b75e3e6ce3d765fd92d8529fa27036c8bf358ea
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-18 10:33:09 +0300

    restyles dashboard ui

--- src/components/ProfileSection.vue
+++ src/components/ProfileSection.vue
@@ -135,7 +135,7 @@ function logout() {
       <div class="flex">
       <!-- <p class="font-semibold text-gray-500 text-xs">Mwebe Raymond </p> -->
        <div class="flex">
-        <span class="font-semibold text-gray-500 text-xs">Mwebe Raymond</span>
+        <span class="font-semibold text-gray-500 text-xs whitespace-nowrap">Mwebe Raymond</span>
        </div>
     <!-- <p class="font-semibold text-gray-500 text-xs">BALANCE: 675,000/=</p> -->
     </div>
