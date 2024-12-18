4609e91257e87cec40d916cde64f3784bc8a5c6f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-18 12:04:55 +0300

    restyles dashboard

--- src/domain/dashboard/views/ServicesStatistics.vue
+++ src/domain/dashboard/views/ServicesStatistics.vue
@@ -235,7 +235,7 @@ function select(tab:string){
 
 <template>
   <div class="flex">
-      <div class="w-2/12 py-1 text-xs">
+      <div class="w-3/12 py-1 text-xs">
         <div :class="(activeTab == 'providers') ? 'tab-active' : 'tab'" @click="select('providers')">
           <div class="w-full py-1 my-auto">
             <label class="p-3">Distribution by Provider</label>
