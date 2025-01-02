88055bfd28383c671a2743fb44e0a1074f1bcd38
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-02 10:19:31 +0300

    restyles dashboard ui

--- src/domain/branches/components/AssignBranchManager.vue
+++ src/domain/branches/components/AssignBranchManager.vue
@@ -163,7 +163,7 @@ const filteredManagers = computed(() => {
                 :disabled="loading"
                 class="px-1 py-0.5 flex bg-red-700 disabled:bg-gray-300 text-white rounded-md"
               >
-              <i class="px-1 fa fa-user-plus"></i> Assign
+              <i class="px-1 py-0.5 fa fa-user-plus"></i> Assign
               </button>
             </td>
           </tr>
