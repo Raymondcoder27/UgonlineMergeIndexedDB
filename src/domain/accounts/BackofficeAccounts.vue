0537b7f16e2eea868c3f7f95fd51e1a45b52ea2a
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-02 13:32:40 +0300

    restyles dashboard ui

--- src/domain/accounts/BackofficeAccounts.vue
+++ src/domain/accounts/BackofficeAccounts.vue
@@ -173,6 +173,7 @@ watch(
             <option value="active">Active</option>
             <option value="blocked">Blocked</option>
           </select> -->
+          
         </div>
          <div class="">
           <button @click="modalOpen = true" class="button btn-sm my-auto" type="button">
