edb5ddbf9b453eccf0762882050af0e7e05a3a68
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-02 11:13:39 +0300

    restyles dashboard

--- src/domain/branches/components/EditBranch.vue
+++ src/domain/branches/components/EditBranch.vue
@@ -101,13 +101,13 @@ watch(
     <p class="text-xl font-bold"> Edit Branch</p>
     <p class="text-sm text-gray-500" v-if="form.name"> <b>{{form.name}}</b> is public good consumed and/or paid for.</p>
     <!-- <form @submit.prevent="submit" class="pt-5"> -->
-      <div v-for="(account, idx) in accountStore.managerAccounts" :key="idx">
+      <div v-for="(account, idx) in store.managerAccounts" :key="idx">
         <div class="flex">
           <div class="cell-full">
             <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Name</label>
             <!-- <input type="text" v-model="form.name" class="noFocus form-element e-input w-full"
                    required /> -->
-            {{branch.name}}
+            <!-- {{branch.name}} -->
           </div>
         </div>
 
@@ -120,65 +120,8 @@ watch(
             {{ account.firstName }} {{ account.lastName }}
           </div>
         </div>
-      <div class="flex">
-        <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Name</label>
-          <!-- <input type="text" v-model="form.name" class="noFocus form-element e-input w-full"
-                 required /> -->
-          {{form.name}}
-        </div>
-      </div>
+ 
 
-      <div class="flex">
-        <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Branch Manager</label>
-          <!-- <select v-model="form.providerId" class="noFocus form-element e-input w-full">
-            <option v-for="(provider, idx) in providerStore.providers" :key="idx" :value="provider.id">{{provider.name}}</option>
-          </select> -->
-        </div>
-      </div>
-<!-- 
-      <div class="flex">
-        <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Accessibility Tier</label>
-          <select v-model="form.accessibilityTier" class="noFocus form-element e-input w-full">
-            <option v-for="(tier, idx) in settingsStore.accessibilityTiers" :key="idx" :value="tier.value">{{tier.text}}</option>
-          </select>
-        </div>
-      </div> -->
-
-      <div class="flex">
-        <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Description</label>
-          <textarea rows="4" v-model="form.description" class="noFocus form-element e-input w-full"
-                 required />
-        </div>
-      </div>
-
-
-      <!-- <div class="flex my-5">
-        <div class="w-full">
-          <div class="flex">
-            <p class="font-bold px-1">Basic User Requirements</p>
-          </div>
-          <div class="flex">
-            <div class="w-9/12" v-if="form.requirements.length > 0">
-              <div class="flex" v-for="(requirement, idx) in form.requirements" :key="idx">
-                <div class="cell-full">
-                  <input type="text" :id="requirement" class="noFocus form-element e-input w-full" v-model="form.requirements[idx]"
-                         required />
-                </div>
-                <i class="fa-solid fa-times text-red-600 pr-2" @click="removeRequirement(idx)"></i>
-              </div>
-            </div>
-            <div class="w-3/12">
-              <button class="button mt-2" type="button" @click="addRequirement">
-                <i class="fa-solid fa-plus"></i> Add Requirement
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> -->
 
       <div class="flex my-2 py-5">
         <div class="w-6/12 px-1">
