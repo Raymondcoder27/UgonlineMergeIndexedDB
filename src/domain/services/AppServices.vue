d6f3d492921a971f6b99422da030a6fb7af736e0
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-02 12:41:32 +0300

    restyles dashboard

--- src/domain/services/AppServices.vue
+++ src/domain/services/AppServices.vue
@@ -78,71 +78,7 @@ const open = (service: any) => {
         @unsubscribe="unsubscribeFromService"
         @open="open"
       /> -->
-      <div class="flex text-xs mt-auto my-3">
-        <!-- <div class="w-full border-t border-b border-gray-50"> -->
-        <div class="w-full border-gray-50">
-          <div class="flex gap-2 items-center mx-4">
-            <!-- Previous Button -->
-            <button
-              class="px-0.5 py-0.5 text-red-600 rounded-md hover:bg-red-700 hover:text-white focus:outline-none focus:ring focus:ring-red-300 disabled:opacity-50 disabled:cursor-not-allowed"
-              :class="{ 'opacity-50 cursor-not-allowed': page <= 1 }"
-              :disabled="page <= 1"
-              @click="previous"
-            >
-              <i class="fa-solid fa-arrow-left"></i>
-            </button>
 
-            <!-- Current Page / Total Pages -->
-            <div class="py-1">
-              <span class="px-2 py-1 bg-primary rounded text-white">{{
-                page
-              }}</span>
-              <label class="mx-1 text-gray-400">/</label>
-              <span class="px-2 py-1 bg-primary-50 rounded text-primary-600">
-                {{ totalPages }}
-              </span>
-            </div>
-            <button
-              class="px-0.5 py-0.5 text-red-600 rounded-md hover:bg-red-700 hover:text-white focus:outline-none focus:ring focus:ring-red-300 disabled:opacity-50 disabled:cursor-not-allowed"
-              :class="{
-                'opacity-50 cursor-not-allowed':
-                  subscribedServices.length < limit,
-              }"
-              :disabled="subscribedServices.length < limit"
-              @click="next"
-            >
-              <i class="fa-solid fa-arrow-right"></i>
-            </button>
-
-            <!-- Jump to Page -->
-            <!-- <label>Page</label>
-          <input
-            type="number"
-            placeholder="Page"
-            class="form-element-lean bg-primary-50 font-bold text-center mx-1 w-6"
-            v-model.number="pageInput"
-            @change="jumpToPage"
-          /> -->
-
-            <!-- Adjust Page Size -->
-            <!-- <label>Page Size</label>
-          <input
-            type="number"
-            placeholder="Page Size"
-            class="form-element-lean bg-primary-50 font-bold text-center mx-1 w-6"
-            v-model.number="limit"
-            @change="changePageSize"
-          />
-
-          Total Records
-          <span
-            class="my-auto mx-2 bg-primary-50 px-3 py-1 rounded text-primary"
-          >
-            Total Services: {{ totalRecords }}
-          </span> -->
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
