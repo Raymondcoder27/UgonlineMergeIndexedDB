b440c7b75676397010007c392849828582ba2214
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-02 16:35:25 +0300

    restyles dashboard ui

--- src/domain/dashboard/views/BranchStatistics.vue
+++ src/domain/dashboard/views/BranchStatistics.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 import BarChart from "@/domain/analytics/components/BarChart.vue";
 import LineChart from "@/domain/analytics/components/LineChart.vue";
 import PieChart from "@/domain/analytics/components/PieChart.vue";
@@ -8,44 +7,42 @@ const branchStore = useBranchStore();
 
 const totalBranches = branchStore.branches?.length || 0;
 
-
 branchStore.fetchBranches();
 
-import type {Ref} from "vue";
-import type {GraphData, Statistic} from "@/domain/analytics/types/chart";
-import {ref} from "vue";
+import type { Ref } from "vue";
+import type { GraphData, Statistic } from "@/domain/analytics/types/chart";
+import { ref } from "vue";
 
 const providerOnboarding: Ref<Array<GraphData>> = ref([
   {
     data: [
       {
-        x: 'October',
-        y: 0
+        x: "October",
+        y: 0,
       },
       {
-        x: 'November',
-        y: 2
+        x: "November",
+        y: 2,
       },
       {
-        x: 'December',
-        y: 6
+        x: "December",
+        y: 6,
       },
       {
-        x: 'January',
-        y: 8
-      }
-    ]
-  }
-])
+        x: "January",
+        y: 8,
+      },
+    ],
+  },
+]);
 
-function labelExtractor(data:Statistic[]){
-  let labels = []
-  for(let i = 0; i < data.length; i++){
-    labels.push(data[i].x)
+function labelExtractor(data: Statistic[]) {
+  let labels = [];
+  for (let i = 0; i < data.length; i++) {
+    labels.push(data[i].x);
   }
   return labels;
 }
-
 </script>
 
 <template>
@@ -56,7 +53,9 @@ function labelExtractor(data:Statistic[]){
           <div class="flex my-2">
             <div class="w-2/12 count">
               <!-- <p class="text-xl font-bold py-2">16</p> -->
- <p class="text-xl font-bold py-2" style="font-size: 18px;">{{ totalBranches }}</p>
+              <p class="text-xl font-bold py-2" style="font-size: 18px">
+                {{ totalBranches }}
+              </p>
               <!-- <p class="text-xs">Total Providers</p> -->
               <p class="text-xs">Total Branches</p>
             </div>
@@ -76,15 +75,39 @@ function labelExtractor(data:Statistic[]){
           <div class="block">
             <div class="flex">
               <div class="w-6/12">
-              <LineChart title="Distribution of Till Operator onboarding" :graph-data="providerOnboarding" :is-horizontal="false"
-                         :labels="labelExtractor(providerOnboarding[0].data)"/>
+                <LineChart
+                  title="Distribution of Till Operator onboarding"
+                  :graph-data="providerOnboarding"
+                  :is-horizontal="false"
+                  :labels="labelExtractor(providerOnboarding[0].data)"
+                />
+              </div>
+              <div class="w-6/12">
+                <PieChart
+                  title="Distribution of Till Operators by Branch"
+                  :data="[22, 14, 18, 13, 9, 6, 4, 8, 17, 4, 5]"
+                  :labels="[
+                    'Branch 1',
+                    'Branch 2',
+                    'Branch 3',
+                    'Branch 4',
+                    'Branch 5',
+                    'Branch 6',
+                    'Branch 7',
+                    'Branch 8',
+                    'Branch 9',
+                    'Branch 10',
+                    'Branch 11',
+                  ]"
+                />
+              </div>
             </div>
             <div class="w-6/12">
-              <PieChart title="Distribution of Till Operators by Branch" :data="[22, 14, 18, 13, 9, 6, 4, 8, 17, 4, 5]" :labels="['Branch 1','Branch 2', 'Branch 3','Branch 4', 'Branch 5','Branch 6','Branch 7', 'Branch 8','Branch 9', 'Branch 10', 'Branch 11']"/>
-            </div>
-            </div>
-            <div class="w-6/12">
-              <PieChart title="Distribution of Till Operators by Gender" :data="[43, 57]" :labels="['Male','Female']"/>
+              <PieChart
+                title="Distribution of Till Operators by Gender"
+                :data="[43, 57]"
+                :labels="['Male', 'Female']"
+              />
             </div>
           </div>
         </div>
@@ -94,7 +117,7 @@ function labelExtractor(data:Statistic[]){
 </template>
 
 <style scoped>
-.count{
+.count {
   @apply py-2 text-center border border-gray-100 rounded bg-gray-10 mx-1 shadow;
 }
 </style>
