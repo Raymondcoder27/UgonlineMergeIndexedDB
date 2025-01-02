9c5f9e989b22c25b5efba264856d1739ab371487
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-02 18:04:01 +0300

    restyles dashboard ui

--- src/domain/accounts/components/EditManager.vue
+++ src/domain/accounts/components/EditManager.vue
@@ -80,7 +80,7 @@ onMounted(() => {
 
 <template>
   <div class="bg-white py-5">
-    <p class="text-xl font-bold">Add Manager</p>
+    <p class="text-xl font-bold">Edit Manager</p>
     <p class="text-sm text-gray-500">The individual responsible for overseeing branch operations, managing staff, and ensuring smooth service delivery to agents and the public.</p>
     <form @submit.prevent="submit" class="pt-5">
       <div class="flex">
