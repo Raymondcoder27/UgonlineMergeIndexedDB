bb8e1d0057f1c62c7838c1dc49d73d76bd821bc2
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-09 15:51:25 +0300

    refactors till operator ui

--- src/agentdomain/apiservices/views/ServiceDetails.vue
+++ src/agentdomain/apiservices/views/ServiceDetails.vue
@@ -35,7 +35,7 @@ function convertDate(date:string){
   <div class="flex flex-col w-screen h-screen bg-neutral-100">
     <div class="flex flex-col h-full w-full">
       <!--< <OptionalHeader /> -->
-      <AgentHeader /> 
+      <!-- <AgentHeader />  -->
       <main class="h-full flex flex-col">
         <div class="flex flex-row h-full">
           <div class="flex flex-col flex-grow">
--- src/domain/services/views/ServiceDetails.vue
+++ src/domain/services/views/ServiceDetails.vue
@@ -7,6 +7,7 @@ import Footer from "@/components/Footer.vue";
 import {useRouter} from "vue-router";
 import ServiceSpecificationDetails from "@/domain/services/views/ServiceSpecificationDetails.vue";
 import OptionalHeader from "@/components/OptionalHeader.vue";
+import AgentHeader from "@/components/AgentHeader.vue";
 
 const store = useServicesStore()
 const loading: Ref<boolean> = ref(false)
@@ -32,7 +33,8 @@ function convertDate(date:string){
 <template>
   <div class="flex flex-col w-screen h-screen bg-neutral-100">
     <div class="flex flex-col h-full w-full">
-      <OptionalHeader />
+      <!-- <OptionalHeader /> -->
+      <AgentHeader />
       <main class="h-full flex flex-col">
         <div class="flex flex-row h-full">
           <div class="flex flex-col flex-grow">
