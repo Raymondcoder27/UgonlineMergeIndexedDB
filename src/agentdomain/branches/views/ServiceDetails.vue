695e9ed5f26f05b21a1b1cddf94fd112e660c5bc
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-09 15:48:04 +0300

    refactors till operator ui

--- src/agentdomain/branches/views/ServiceDetails.vue
+++ src/agentdomain/branches/views/ServiceDetails.vue
@@ -33,7 +33,8 @@ function convertDate(date:string){
 <template>
   <div class="flex flex-col w-screen h-screen bg-neutral-100">
     <div class="flex flex-col h-full w-full">
-      <OptionalHeader />
+     <!-- <OptionalHeader /> -->
+      <AgentHeader />
       <main class="h-full flex flex-col">
         <div class="flex flex-row h-full">
           <div class="flex flex-col flex-grow">
