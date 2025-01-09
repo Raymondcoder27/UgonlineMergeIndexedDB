2a01857a46df2b0022df9474f54ac104bdb2dc72
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-09 14:43:30 +0300

    init

--- src/agentdomain/composer/FormComposer.vue
+++ src/agentdomain/composer/FormComposer.vue
@@ -160,14 +160,7 @@ watch(
           <div class="w-11/12 border border-blue-200 bg-blue-50 p-3 text-sm rounded text-blue-600">
             <p class="my-auto"> <i class="fa-solid fa-info-circle"></i> <label class="font-bold">Form Builder</label> <br/>
               <span class="text-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-              desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-              desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-              It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+              dummy text ever since the 1500s, when an unknown printer took a galley of type. remaining essentially unchanged.
               </span>
             </p>
           </div>
