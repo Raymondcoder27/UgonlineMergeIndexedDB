958fbc3cd3c77b72c93cf9edb49d55c9fc71e05a
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-13 12:46:46 +0300

    refactors till operator ui

--- src/agentdomain/apisubmissions/ServiceApplications.vue
+++ src/agentdomain/apisubmissions/ServiceApplications.vue
@@ -25,6 +25,29 @@ let providerId = ref("");
 let status = ref("");
 const notify = useNotificationsStore();
 
+
+
+const showPagination = computed(() => totalRecords.value >= limit.value);
+
+const pageInput = ref(1);
+const changePageSize = () => {
+  page.value = 1;
+  fetchSubmissions();
+};
+const showPagination = computed(() => totalRecords.value >= limit.value);
+
+const jumpToPage = () => {
+  if (pageInput.value > totalPages.value) {
+    page.value = totalPages.value;
+  } else if (pageInput.value < 1) {
+    page.value = 1;
+  } else {
+    page.value = pageInput.value;
+  }
+  fetchSubmissions();
+};
+
+
 onMounted(() => {
   fetch();
   providerStore
@@ -374,7 +397,73 @@ watch(
       </table>
     </div>
     <div class="flex">
-      <div class="w-full">
+      <div
+        v-if="showPagination"
+        class="flex text-xs mt-auto justify-center items-center"
+      >
+        <div class="w-full border-t border-b border-gray-50">
+          <div class="flex gap-2 items-center">
+            <!-- Previous Button -->
+            <button
+              class="px-1 py-0.5 text-red-600 rounded-md hover:bg-red-700 hover:text-white focus:outline-none focus:ring focus:ring-red-300 disabled:opacity-50 disabled:cursor-not-allowed"
+              :class="{ 'opacity-50 cursor-not-allowed': page <= 1 }"
+              :disabled="page <= 1"
+              @click="previous"
+            >
+              <i class="fa-solid fa-arrow-left"></i>
+            </button>
+
+            <!-- Current Page / Total Pages -->
+            <div class="py-1">
+              <span class="px-2 py-1 bg-primary rounded text-white">{{
+                page
+              }}</span>
+              <label class="mx-1 text-gray-400">/</label>
+              <span class="px-2 py-1 bg-primary-50 rounded text-primary-600">
+                {{ totalPages }}
+              </span>
+            </div>
+            <button
+              class="px-1 py-0.5 text-red-600 rounded-md hover:bg-red-700 hover:text-white focus:outline-none focus:ring focus:ring-red-300 disabled:opacity-50 disabled:cursor-not-allowed"
+              :class="{
+                'opacity-50 cursor-not-allowed': submissions.length < limit,
+              }"
+              :disabled="submissions.length < limit"
+              @click="next"
+            >
+              <i class="fa-solid fa-arrow-right"></i>
+            </button>
+
+            <!-- Jump to Page -->
+            <label>Page</label>
+            <input
+              type="number"
+              placeholder="Page"
+              class="form-element-lean bg-primary-50 font-bold text-center mx-1 w-12"
+              v-model.number="pageInput"
+              @change="jumpToPage"
+            />
+
+            <!-- Adjust Page Size -->
+            <label>Page Size</label>
+            <input
+              type="number"
+              placeholder="Page Size"
+              class="form-element-lean bg-primary-50 font-bold text-center mx-1 w-12"
+              v-model.number="limit"
+              @change="changePageSize"
+            />
+
+            <!-- Total Records -->
+            <span
+              class="my-auto mx-2 bg-primary-50 px-3 py-1 rounded text-primary"
+            >
+              Total Records: {{ totalRecords }}
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="w-full">
         <div
           class="flex"
           v-if="limit == store.applications?.length || page > 1"
@@ -401,7 +490,7 @@ watch(
             Next <i class="fa-solid fa-arrow-right"></i>
           </button>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 
