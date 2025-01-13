b145bab3ad8d3d3d00941efd3c284cc5b3d53cc6
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-13 11:46:26 +0300

    init

--- src/agentdomain/apisubmissions/stores/index.ts
+++ src/agentdomain/apisubmissions/stores/index.ts
@@ -1,35 +1,35 @@
 
-import {defineStore} from "pinia";
-import {ref, type Ref} from "vue";
+import { defineStore } from "pinia";
+import { ref, type Ref } from "vue";
 import api from "@/config/api";
-import type {ApplicationStatistics, ClientApplication} from "@/agentdomain/apisubmissions/types";
+import type { ApplicationStatistics, ClientApplication } from "@/agentdomain/apisubmissions/types";
 
 export const useApplications = defineStore("applications", () => {
   const applications: Ref<ClientApplication[] | undefined> = ref()
   const applicationStatistics: Ref<ApplicationStatistics | undefined> = ref()
 
-  const fetchApplications = async (trackingNo:string, serviceId:string, providerId:string, status:string, page:number, limit:number) => {
-    let path = "/registry/v1/applications?page="+page+"&limit="+limit
-    if(trackingNo){
-      path += "&tracking_no="+trackingNo
+  const fetchApplications = async (trackingNo: string, serviceId: string, providerId: string, status: string, page: number, limit: number) => {
+    let path = "/registry/v1/applications?page=" + page + "&limit=" + limit
+    if (trackingNo) {
+      path += "&tracking_no=" + trackingNo
     }
-    if(serviceId){
-      path += "&service_id="+serviceId
+    if (serviceId) {
+      path += "&service_id=" + serviceId
     }
-    if(providerId){
-      path += "&provider_id="+providerId
+    if (providerId) {
+      path += "&provider_id=" + providerId
     }
-    if(status){
-      path += "&status="+status
+    if (status) {
+      path += "&status=" + status
     }
-    return api.get(path).then((response:any) => {
+    return api.get(path).then((response: any) => {
       applications.value = response.data.data
       console.log(response.data.data)
     })
   }
 
-  const fetchApplicationStats = async (status:boolean) => {
-    return api.get("/registry/v1/applications/stats?status="+status).then((response:any) => {
+  const fetchApplicationStats = async (status: boolean) => {
+    return api.get("/registry/v1/applications/stats?status=" + status).then((response: any) => {
       applicationStatistics.value = response.data.data
       console.log(response.data.data)
     })
