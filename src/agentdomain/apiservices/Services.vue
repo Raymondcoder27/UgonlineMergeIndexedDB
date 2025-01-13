b0e6486c8cf13e856696daf4213af07b54d94c9d
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-13 12:34:38 +0300

    refactors till operator ui

--- src/agentdomain/apiservices/Services.vue
+++ src/agentdomain/apiservices/Services.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import AppModal from "@/components/AppModal.vue";
-import {onMounted, type Ref, ref, watch} from "vue";
+import { onMounted, type Ref, ref, watch } from "vue";
 import CreateService from "@/agentdomain/apiservices/components/CreateService.vue";
-import {useServicesStore} from "@/agentdomain/apiservices/stores";
-import type {Service} from "@/agentdomain/apiservices/types";
+import { useServicesStore } from "@/agentdomain/apiservices/stores";
+import type { Service } from "@/agentdomain/apiservices/types";
 import EditService from "@/agentdomain/apiservices/components/EditService.vue";
 import ServiceSpecificationDetails from "@/agentdomain/apiservices/views/ServiceSpecificationDetails.vue";
 import moment from "moment/moment";
 import router from "@/router";
-import {useProviderStore} from "@/agentdomain/providers/stores";
+import { useProviderStore } from "@/agentdomain/providers/stores";
 import CategorySelector from "@/agentdomain/settings/components/CategorySelector.vue";
-import {useNotificationsStore} from "@/stores/notifications";
-import type {ApiError} from "@/types";
+import { useNotificationsStore } from "@/stores/notifications";
+import type { ApiError } from "@/types";
 import TableLoader from "@/components/TableLoader.vue";
 
 const store = useServicesStore();
@@ -21,53 +21,55 @@ const specModalOpen: Ref<boolean> = ref(false);
 const editModalOpen: Ref<boolean> = ref(false);
 const page: Ref<number> = ref(1);
 const limit: Ref<number> = ref(16);
-const loading: Ref<boolean> = ref(false)
-const selectedService: Ref<string> = ref("")
-let providerId = ref("")
-let status = ref("")
-const notify =  useNotificationsStore()
+const loading: Ref<boolean> = ref(false);
+const selectedService: Ref<string> = ref("");
+let providerId = ref("");
+let status = ref("");
+const notify = useNotificationsStore();
 
-const providerStore = useProviderStore()
+const providerStore = useProviderStore();
 onMounted(() => {
-  loading.value = true
-  store.fetchServices(page.value, limit.value)
+  loading.value = true;
+  store
+    .fetchServices(page.value, limit.value)
+    .then(() => (loading.value = false))
+    .catch((error: ApiError) => {
+      loading.value = false;
+      notify.error(error.response.data.message);
+    });
+  if (providerStore.providers == undefined) {
+    providerStore
+      .fetchProviders(1, 35)
       .then(() => (loading.value = false))
-      .catch((error:ApiError) => {
-        loading.value = false
-        notify.error(error.response.data.message)
-      })
-  if(providerStore.providers == undefined){
-    providerStore.fetchProviders(1, 35)
-        .then(() => (loading.value = false))
-        .catch(() => {
-          loading.value = false
-        })
+      .catch(() => {
+        loading.value = false;
+      });
   }
-})
+});
 
-function edit(service:Service) {
-  localStorage.setItem("service", JSON.stringify(service))
+function edit(service: Service) {
+  localStorage.setItem("service", JSON.stringify(service));
   editModalOpen.value = true;
 }
 
-function spec(service:Service) {
-  selectedService.value = service.id
-  localStorage.clear()
-  localStorage.setItem("service", JSON.stringify(service))
+function spec(service: Service) {
+  selectedService.value = service.id;
+  localStorage.clear();
+  localStorage.setItem("service", JSON.stringify(service));
   specModalOpen.value = true;
 }
 
-function open(service:Service) {
-  router.push({name:"api-service-details", params:{id:service.id}})
+function open(service: Service) {
+  router.push({ name: "api-service-details", params: { id: service.id } });
 }
 
-function tag(service:Service) {
-  selectedService.value = service.id
-  categoryModalOpen.value = true
+function tag(service: Service) {
+  selectedService.value = service.id;
+  categoryModalOpen.value = true;
 }
 
-function convertDateTime(date:string){
-  return moment(date).format("DD-MM-YYYY HH:mm:ss")
+function convertDateTime(date: string) {
+  return moment(date).format("DD-MM-YYYY HH:mm:ss");
 }
 
 function close() {
@@ -77,11 +79,11 @@ function close() {
 }
 
 watch(
-    () => providerId.value,
-    (id:any) => {
-      console.log(id)
-    },
-    { deep: true }
+  () => providerId.value,
+  (id: any) => {
+    console.log(id);
+  },
+  { deep: true }
 );
 
 // watch state of the modal
@@ -91,7 +93,7 @@ watch(
     if (!isOpen) {
       // do something if that's something you're interested in
     }
-  },
+  }
 );
 </script>
 
@@ -115,7 +117,13 @@ watch(
           />
           <select class="filter-element w-2/12 e-select" v-model="providerId">
             <option :value="null">- Select Provider -</option>
-            <option v-for="(provider, idx) in providerStore.providers" :key="idx" :value="provider.id">{{provider.name}}</option>
+            <option
+              v-for="(provider, idx) in providerStore.providers"
+              :key="idx"
+              :value="provider.id"
+            >
+              {{ provider.name }}
+            </option>
           </select>
           <select class="filter-element w-2/12 e-select" v-model="status">
             <option :value="null">- Select Status -</option>
@@ -149,7 +157,7 @@ watch(
           </tr>
           <tr>
             <th colspan="12">
-              <TableLoader v-if="loading"/>
+              <TableLoader v-if="loading" />
             </th>
           </tr>
         </thead>
@@ -161,37 +169,52 @@ watch(
           >
             <td width="10px">{{ idx + 1 }}.</td>
             <td>
-              <label class=" cursor-pointer font-bold hover:text-primary-700 mx-2" >
+              <label
+                class="cursor-pointer font-bold hover:text-primary-700 mx-2"
+              >
                 <span class="hover:underline" @click="open(service)">
                   {{ service.name }}
                 </span>
-                <i class="fa-solid fa-link p-1 mx-1 text-gray-600 bg-gray-50 hover:text-primary-700"  @click="tag(service)"></i>
+                <i
+                  class="fa-solid fa-link p-1 mx-1 text-gray-600 bg-gray-50 hover:text-primary-700"
+                  @click="tag(service)"
+                ></i>
               </label>
             </td>
             <td>
-              <label>{{ service.providerName}}</label>
+              <label>{{ service.providerName }}</label>
             </td>
             <td>
-              <label>{{ service.accessibilityTier}}</label>
+              <label>{{ service.accessibilityTier }}</label>
             </td>
             <td class="text-center">
               <i
                 :class="
                   service.currentVersionId
                     ? 'text-green-600 fa-solid fa-check'
-                    : 'text-red-600 fa-solid fa-times'"
+                    : 'text-red-600 fa-solid fa-times'
+                "
               ></i>
             </td>
             <td class="text-center">
-              <span>{{service.status}}</span>
+              <span>{{ service.status }}</span>
             </td>
             <!-- <td class="text-center">
               <span class="text-xs">{{ convertDateTime(service.createdAt.Time) }}</span>
             </td> -->
             <td class="text-center">
-              <i class="fa-solid fa-eye p-1 mx-1 text-blue-600 bg-blue-100 border border-blue-200  hover:text-blue-700"  @click="open(service)"></i>
-              <i class="fa-solid fa-pen p-1 mx-1 text-green-600 bg-green-100 border border-green-200 hover:text-green-700"  @click="edit(service)"></i>
-              <i class="fa-solid fa-sliders p-1 mx-1 text-primary-700 bg-primary-100 border border-primary-300 hover:text-primary-900"  @click="spec(service)"></i>
+              <i
+                class="fa-solid fa-eye p-1 mx-1 text-blue-600 bg-blue-100 border border-blue-200 hover:text-blue-700"
+                @click="open(service)"
+              ></i>
+              <i
+                class="fa-solid fa-pen p-1 mx-1 text-green-600 bg-green-100 border border-green-200 hover:text-green-700"
+                @click="edit(service)"
+              ></i>
+              <i
+                class="fa-solid fa-sliders p-1 mx-1 text-primary-700 bg-primary-100 border border-primary-300 hover:text-primary-900"
+                @click="spec(service)"
+              ></i>
             </td>
           </tr>
         </tbody>
@@ -203,26 +226,26 @@ watch(
   <AppModal v-model="modalOpen" xl2>
     <!-- Put here whatever makes you smile -->
     <!-- Chances are high that you're starting with a form -->
-    <CreateService @cancel="close"/>
+    <CreateService @cancel="close" />
     <!-- That's also okay -->
   </AppModal>
 
   <AppModal v-model="categoryModalOpen" xl2>
     <!-- Put here whatever makes you smile -->
     <!-- Chances are high that you're starting with a form -->
-    <CategorySelector :service-id="selectedService" @cancel="close"/>
+    <CategorySelector :service-id="selectedService" @cancel="close" />
     <!-- That's also okay -->
   </AppModal>
 
   <AppModal v-model="specModalOpen" xl6>
     <!-- Put here whatever makes you smile -->
-    <ServiceSpecificationDetails :id="selectedService"/>
+    <ServiceSpecificationDetails :id="selectedService" />
     <!-- That's also okay -->
   </AppModal>
 
   <AppModal v-model="editModalOpen" xl2>
     <!-- Put here whatever makes you smile -->
-    <EditService  @cancel="close"/>
+    <EditService @cancel="close" />
     <!-- That's also okay -->
   </AppModal>
   <!-- /Modal -->
