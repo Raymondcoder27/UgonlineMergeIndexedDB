2203ca342fa924fe9761ddc84335fff6d7ac7d73
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-13 10:51:13 +0300

    init

--- src/components/AgentTopMenu.vue
+++ src/components/AgentTopMenu.vue
@@ -27,7 +27,7 @@ const topMenu: TopMenuLink[] = [
   //   label: "Services",
   //   iconClass: "fa-solid fa-home",
   // },
-   {
+  {
     name: "api-services",
     label: "Services",
     iconClass: "fa-solid fa-list",
@@ -44,13 +44,12 @@ const topMenu: TopMenuLink[] = [
     // iconClass: "fa-solid fa-upload",
     // iconClass: "fa fa-paper-plane",
     iconClass: "fa fa-check-circle",
-
   },
-   {
+  {
     name: "agent-app-ledger",
     label: "Ledger",
     iconClass: "fa-solid fa-money-check",
-  }
+  },
   //  {
   //   name: "api-services",
   //   label: "Services",
@@ -58,37 +57,34 @@ const topMenu: TopMenuLink[] = [
   // }
 ];
 
-
 const filter: IGoFilter = reactive({
   limit: 100,
   offset: 0,
-  page:0,
+  page: 0,
   sort: [
     {
       field: "firstname",
-      order: "ASC"
-    }
+      order: "ASC",
+    },
   ],
   filter: [
     {
       field: "firstname",
       operand: "",
-      operator: "CONTAINS"
+      operator: "CONTAINS",
     },
     {
       field: "username",
       operand: "",
-      operator: "CONTAINS"
+      operator: "CONTAINS",
     },
     {
       field: "phone",
       operand: "",
-      operator: "CONTAINS"
+      operator: "CONTAINS",
     },
-  ]
-})
-
-
+  ],
+});
 
 const navigate: (link: TopMenuLink) => void = (link: TopMenuLink) => {
   //hide sidemenu if viewed on a mobile device
@@ -168,7 +164,8 @@ function logout() {
 
 <template>
   <!-- Desktop Menu: Centered Horizontally -->
-  <div v-if="!isMobile" class="flex justify-center text-center w-full"> <!-- Center the entire container for desktop -->
+  <div v-if="!isMobile" class="flex justify-center text-center w-full">
+    <!-- Center the entire container for desktop -->
     <Transition
       enter-from-class="-translate-x-0"
       enter-active-class="transition-all duration-300 ease-in-out"
@@ -179,7 +176,9 @@ function logout() {
         :class="{ 'w-full': menuOpen, 'group w-auto hover:w-full': !menuOpen }"
         class="relative bg-white flex items-center justify-between z-10 pt-2"
       >
-        <div class="flex flex-row items-center w-full space-x-6 px-4 py-2 text-sm">
+        <div
+          class="flex flex-row items-center w-full space-x-6 px-4 py-2 text-sm"
+        >
           <!-- User Profile and Logout Section -->
 
           <!-- Link Section-->
@@ -199,7 +198,9 @@ function logout() {
               "
             >
               <i class="my-auto text-sm fa-fw" :class="link.iconClass"></i>
-              <span class="my-auto text-sm font-semibold">{{ link.label }}</span>
+              <span class="my-auto text-sm font-semibold">{{
+                link.label
+              }}</span>
             </div>
           </div>
 
