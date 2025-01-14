961cd1f5f03d8da9a5c30c46657e46c51089edaf
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-14 18:06:59 +0300

    init

--- src/domain/finances/FloatRequests.vue
+++ src/domain/finances/FloatRequests.vue
@@ -429,7 +429,6 @@ onMounted(() => {
                 <!-- <label> -->
                 <span
                   class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-white bg-blue-600 hover:text-white hover:bg-blue-700"
-                   @click="view(request)"
                 >
                   <!-- <i class="fa-solid fa-check"></i>
                   Approved</span
