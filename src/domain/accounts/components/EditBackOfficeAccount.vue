d685999b597807a8c1c48186fddf10434f3e80c9
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-14 14:55:02 +0300

    refactors till operator ui

--- src/domain/accounts/components/EditBackOfficeAccount.vue
+++ src/domain/accounts/components/EditBackOfficeAccount.vue
@@ -2,11 +2,13 @@
 
 import {onMounted, reactive, type Ref, ref} from "vue";
 import {useProviderStore} from "@/domain/providers/stores";
+import { useAccountStore } from "@/domain/auth/stores";
 import type {CreateServiceProvider} from "@/domain/providers/types";
 import {useNotificationsStore} from "@/stores/notifications";
 import type {ApiError} from "@/types";
 
 const store = useProviderStore()
+const accountStore = useAccountStore()
 const loading: Ref<boolean> = ref(false)
 const notify =useNotificationsStore()
 
