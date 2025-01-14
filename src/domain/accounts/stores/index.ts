15378e36466362230ea12011f351fc163328aff0
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-14 15:28:15 +0300

    refactors till operator ui

--- src/domain/accounts/stores/index.ts
+++ src/domain/accounts/stores/index.ts
@@ -126,13 +126,13 @@ export const useAccounts = defineStore("user-management", () => {
       branch.manager = payload.managerId;
     }
 
-    saveManagerToLocalStorage(payload);
+    saveManagerToLocalStorage();
   }
 
   const localStorageManagerAccount = ref<ManagerAccount>();
 
   // Save manager to local storage
-  const saveManagerToLocalStorage = (payload: string) => {
+  const saveManagerToLocalStorage = () => {
     // localStorage.setItem('branchManagerFloatBalance', JSON.stringify(localStorageManagerAccount.value))
     localStorage.setItem('branchManagerAccount', JSON.stringify(localStorageManagerAccount.value))
   }
