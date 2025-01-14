da2368132df4f42cc3704a3780eaa2d259c92196
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-14 16:24:16 +0300

    refactors till operator ui

--- src/domain/branches/components/EditBranch.vue
+++ src/domain/branches/components/EditBranch.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-
-import {onMounted, reactive, type Ref, ref} from "vue";
-import {useProviderStore} from "@/domain/providers/stores";
+import { onMounted, reactive, type Ref, ref } from "vue";
+import { useProviderStore } from "@/domain/providers/stores";
 import { useAccountStore } from "@/domain/auth/stores";
-import type {CreateServiceProvider} from "@/domain/providers/types";
+import type { CreateServiceProvider } from "@/domain/providers/types";
 import type { AssignManager } from "@/types";
-import {useNotificationsStore} from "@/stores/notifications";
-import type {ApiError} from "@/types";
+import { useNotificationsStore } from "@/stores/notifications";
+import type { ApiError } from "@/types";
 
-const store = useProviderStore()
-const accountStore = useAccountStore()
-const loading: Ref<boolean> = ref(false)
-const notify =useNotificationsStore()
+const store = useProviderStore();
+const accountStore = useAccountStore();
+const loading: Ref<boolean> = ref(false);
+const notify = useNotificationsStore();
 
 // let form: CreateServiceProvider = reactive({
 //   name: "",
@@ -28,13 +27,13 @@ let form: CreateServiceProvider = reactive({
   name: "",
   displayName: "",
   displayLogo: null,
-  providerType:"GOVERNMENT",
+  providerType: "GOVERNMENT",
   physicalAddress: "",
-  inquiryEmail:"",
-  inquiryPhoneNumber:"",
-  username: ""
-})
-const emit = defineEmits(['cancel'])
+  inquiryEmail: "",
+  inquiryPhoneNumber: "",
+  username: "",
+});
+const emit = defineEmits(["cancel"]);
 
 // onMounted(() => {
 //   let data = JSON.parse(<string>localStorage.getItem("provider"))
@@ -48,16 +47,16 @@ const emit = defineEmits(['cancel'])
 // })
 
 onMounted(() => {
-//   let data = JSON.parse(<string>localStorage.getItem("provider"))
-  let data = JSON.parse(<string>localStorage.getItem("backofficeAccount"))
+  //   let data = JSON.parse(<string>localStorage.getItem("provider"))
+  let data = JSON.parse(<string>localStorage.getItem("backofficeAccount"));
 
-  form.name = data.name
-  form.displayName = data.displayName
-  form.physicalAddress = data.physicalAddress
-  form.inquiryEmail = data.inquiryEmail
-  form.inquiryPhoneNumber = data.inquiryPhoneNumber
-  form.username = data.username
-})
+  form.name = data.name;
+  form.displayName = data.displayName;
+  form.physicalAddress = data.physicalAddress;
+  form.inquiryEmail = data.inquiryEmail;
+  form.inquiryPhoneNumber = data.inquiryPhoneNumber;
+  form.username = data.username;
+});
 
 // function submit(){
 //   loading.value = true
@@ -92,18 +91,18 @@ function submit(userId: string) {
   //   userId: userId,
   //   branchId: branchId,
   // };
-  let data = JSON.parse(<string>localStorage.getItem("provider"))
+  let data = JSON.parse(<string>localStorage.getItem("provider"));
 
-  let id = data.id
+  let id = data.id;
   let payload = {
-    name:form.name,
-    display_name:form.displayName,
-    inquiry_email:form.inquiryEmail,
-    provider_type:form.providerType,
-    inquiry_phone_number:form.inquiryPhoneNumber,
-    physical_address:form.physicalAddress,
-    username:form.username
-  }
+    name: form.name,
+    display_name: form.displayName,
+    inquiry_email: form.inquiryEmail,
+    provider_type: form.providerType,
+    inquiry_phone_number: form.inquiryPhoneNumber,
+    physical_address: form.physicalAddress,
+    username: form.username,
+  };
 
   loading.value = true;
   store.assignManager(payload.userId, payload.branchId);
@@ -113,26 +112,39 @@ function submit(userId: string) {
   emit("managerAssigned");
   loading.value = false;
 }
-
 </script>
 
 <template>
   <div class="bg-white py-5">
     <p class="text-xl font-bold">Edit Service Provider</p>
-    <p class="text-sm text-gray-500" v-if="form.name"><b>{{form.name}}</b> provides a services consumed by the general public of Uganda.</p>
+    <p class="text-sm text-gray-500" v-if="form.name">
+      <b>{{ form.name }}</b> provides a services consumed by the general public
+      of Uganda.
+    </p>
     <form @submit.prevent="submit" class="pt-5">
       <div class="flex">
         <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Provider Name</label>
-          <input type="text" v-model="form.name" class="noFocus form-element e-input w-full"
-                 required />
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Provider Name</label
+          >
+          <input
+            type="text"
+            v-model="form.name"
+            class="noFocus form-element e-input w-full"
+            required
+          />
         </div>
       </div>
 
       <div class="flex">
         <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Provider Type</label>
-          <select v-model="form.providerType" class="noFocus form-element e-input w-full">
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Provider Type</label
+          >
+          <select
+            v-model="form.providerType"
+            class="noFocus form-element e-input w-full"
+          >
             <option value="GOVERNMENT">Government Entity</option>
             <option value="PRIVATE">Private Company</option>
           </select>
@@ -141,33 +153,63 @@ function submit(userId: string) {
 
       <div class="flex">
         <div class="cell">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Display Name</label>
-          <input type="text" v-model="form.displayName" class="noFocus form-element e-input w-full"
-                 required />
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Display Name</label
+          >
+          <input
+            type="text"
+            v-model="form.displayName"
+            class="noFocus form-element e-input w-full"
+            required
+          />
         </div>
         <div class="cell">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Username</label>
-          <input type="text" v-model="form.username" class="noFocus form-element e-input w-full"
-                 required />
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Username</label
+          >
+          <input
+            type="text"
+            v-model="form.username"
+            class="noFocus form-element e-input w-full"
+            required
+          />
         </div>
       </div>
 
       <p class="text-sm font-bold pt-5">Provider Inquiry Details</p>
       <div class="flex">
         <div class="cell">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Inquiry Email Address</label>
-          <input type="email" v-model="form.inquiryEmail" class="noFocus form-element e-input w-full"/>
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Inquiry Email Address</label
+          >
+          <input
+            type="email"
+            v-model="form.inquiryEmail"
+            class="noFocus form-element e-input w-full"
+          />
         </div>
         <div class="cell">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Inquiry Phone Number</label>
-          <input type="tel" v-model="form.inquiryPhoneNumber" class="noFocus form-element e-input w-full"/>
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Inquiry Phone Number</label
+          >
+          <input
+            type="tel"
+            v-model="form.inquiryPhoneNumber"
+            class="noFocus form-element e-input w-full"
+          />
         </div>
       </div>
       <div class="flex">
         <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Physical Address</label>
-          <textarea v-model="form.physicalAddress" class="noFocus form-element e-input w-full" cols="4"
-                    placeholder="Address Description"/>
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Physical Address</label
+          >
+          <textarea
+            v-model="form.physicalAddress"
+            class="noFocus form-element e-input w-full"
+            cols="4"
+            placeholder="Address Description"
+          />
         </div>
       </div>
 
@@ -181,7 +223,10 @@ function submit(userId: string) {
           <button class="button" type="submit">
             <i class="fa-solid fa-hand-pointer"></i> Submit
             <span class="lds-ring mx-1" v-if="loading">
-              <div></div><div></div><div></div><div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
             </span>
           </button>
         </div>
