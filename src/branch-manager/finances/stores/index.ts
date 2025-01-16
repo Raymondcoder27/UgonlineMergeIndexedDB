30c9846d435c4e19ef4fd69b3c2b993d6ff6de01
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-16 16:55:44 +0300

    ignores node modules

--- src/branch-manager/finances/stores/index.ts
+++ src/branch-manager/finances/stores/index.ts
@@ -1,345 +1,165 @@
-// branch-manager/finances/stores.ts
-
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { db } from "@/branch-manager/db/db";
 import type { Transaction, FloatLedger, BackofficeUser, TillOperator, FloatAllocation, FloatRequest } from "@/branch-manager/finances/types";
 import type { AllocateFloat } from "@/types";
 
 export const useBilling = defineStore("billing", () => {
-  // Dummy data for testing
-
-  // use this for dummy transactions
-  // <tr class="text-left">
-  //           <!-- <th>#</th> -->
-  //           <th>Tracking Number</th>
-  //           <th>Service</th>
-  //           <th>Provider</th>
-  //           <th>Till</th>
-  //           <!-- <th>Transaction Type</th> -->
-  //           <th>Fee</th>
-  //           <!-- <th>Status</th> -->
-  //           <th>Date</th>
-  //           <!-- <th>Actions</th> -->
-  //         </tr>
-
-  const dummyTransactions: Transaction[] = [
-    {
-      id: 1, trackingNumber: "TA123456",
-      service: "Company Name Reservation", provider: "URSB", till: "Till 001",
-      fee: 25000, date: "2021-09-01", status: "success"
-    },
-    {
-      id: 2, trackingNumber: "TB123457",
-      service: "Create Postal Account", provider: "Posta Uganda",
-      till: "Till 002", fee: 20000, date: "2021-09-02", status: "failed"
-    },
-    {
-      id: 3, trackingNumber: "TC123458",
-      service: "National ID registration", provider: "NIRA",
-      till: "Till 003", fee: 35000, date: "2021-09-03", status: "pending"
-    },
-  ];
-
-
-  // use this for dummy float requests
-  // <th class="text-left">Date</th>
-  // <th class="text-left">Name</th>
-  // <th class="text-left"> till</th>
-  // <th class="text-left">Amount</th>
-  // <th class="text-left">Actions</th>
-
-  const dummyFloatRequests: FloatRequest[] = [
-    { id: 1, requestDate: "2021-09-01", amount: 12000000, status: "pending", till: "Till 1", approvedBy: null, requesterName: "", createdAt: "", date: "" },
-    { id: 4, requestDate: "2021-09-04", amount: 10000000, status: "pending", till: "Till 4", approvedBy: null, requesterName: "", createdAt: "", date: "" },
-    { id: 2, requestDate: "2021-09-02", amount: 18000000, status: "approved", till: "Till 2", approvedBy: "Manager One", requesterName: "", createdAt: "", date: "" },
-    { id: 3, requestDate: "2021-09-03", amount: 9000000, status: "rejected", till: "Till 3", approvedBy: null, requesterName: "", createdAt: "", date: "" },
-  ];
-
-  const dummyFloatLedgers: FloatLedger[] = [
-    { id: 1, date: "2021-09-01", description: "Recharge", amount: 20000000, balance: 20000000 },
-    // { id: 2, date: "2021-09-02", description: "Till 1", amount: -20000000, balance: 300000000 },
-  ];
-
-  const dummyBackofficeUsers: BackofficeUser[] = [
-    { id: 1, username: "admin1", fullName: "Jack Mwebe", role: "Administrator", till: "Till 1", status: "Active" },
-    { id: 2, username: "manager1", fullName: "Katamba Johnson", role: "Manager", till: "Till 2", status: "Active" },
-    { id: 3, username: "admin2", fullName: "Kasule Ronald", role: "Administrator", till: "Till 3", status: "Inactive" },
-  ];
-
-  // dummy  till manager data
-  const dummyTillOperators: TillOperator[] = [
-    { id: 1, username: "manager1", fullName: "Manager User One", role: "Manager", till: "Till 1", status: "Active" },
-    { id: 2, username: "manager2", fullName: "Manager User Two", role: "Manager", till: "Till 2", status: "Active" },
-    { id: 3, username: "manager3", fullName: "Manager User Three", role: "Manager", till: "Till 3", status: "Inactive" },
-  ];
-
-  // dummy float assignment data
-  const dummyFloatAllocations: FloatAllocation[] = [
-    { id: 1, dateAssigned: "2021-09-01", amount: 10000000, status: "Allocated", till: "Till 1" },
-    { id: 2, dateAssigned: "2021-09-02", amount: 21000000, status: "pending", till: "Till 2" },
-    { id: 3, dateAssigned: "2021-09-03", amount: 17000000, status: "failed", till: "Till 3" },
-  ];
-
-
   // State variables
-  const transactions = ref<Transaction[]>(dummyTransactions); // Use dummy data for now
+  const transactions = ref<Transaction[]>([]);
   const totalAmount = ref(600); // Set a test value
   const totalBalance = ref(3000); // Set a test value
-  const floatLedgers = ref<FloatLedger[]>(dummyFloatLedgers); // Use dummy data for now
-  const backofficeUsers = ref<BackofficeUser[]>(dummyBackofficeUsers);
-  const tillOperators = ref<TillOperator[]>(dummyTillOperators);
-  const floatAllocations = ref<FloatAllocation[]>(dummyFloatAllocations);
-  // const floatRequests = ref<FloatRequest[]>(dummyFloatRequests);
+  const floatLedgers = ref<FloatLedger[]>([]);
+  const backofficeUsers = ref<BackofficeUser[]>([]);
+  const tillOperators = ref<TillOperator[]>([]);
+  const floatAllocations = ref<FloatAllocation[]>([]);
+  const floatRequests = ref<FloatRequest[]>([]);
+  const tillOperatorFloatBalance = ref(0);
 
-
-  const floatRequests = JSON.parse(localStorage.getItem('floatRequestToBranchManagerLocalStorage') || '[]');
-
-  if (floatRequests) {
-    floatRequests.value = floatRequests;
+  // Fetch data and populate state variables
+  async function fetchTransactions() {
+    try {
+      const storedTransactions = await db.getAll<Transaction>();
+      transactions.value = storedTransactions;
+    } catch (error) {
+      console.error("Error fetching transactions from IndexedDB:", error);
+    }
   }
 
-
-  // Actions to fetch data
-  async function fetchTransactions(filter: any) {
-    // Simulate API call
-    // const response = await fetch(`/api/transactions?limit=${filter.limit}&page=${filter.page}`);
-    // const data = await response.json();
-    // Use dummy data for now
-    transactions.value = dummyTransactions;
-    // totalAmount.value = 600;  // Set a test value
-    // totalBalance.value = 300000000; // Set a test value
+  async function fetchFloatLedgers() {
+    try {
+      const storedLedgers = await db.getAll<FloatLedger>();
+      floatLedgers.value = storedLedgers;
+    } catch (error) {
+      console.error("Error fetching float ledgers from IndexedDB:", error);
+    }
   }
 
-  async function fetchFloatLedgers(filter: any) {
-    // Simulate API call
-    // const response = await fetch(`/api/float-ledgers?limit=${filter.limit}&page=${filter.page}`);
-    // const data = await response.json();
-    // Use dummy data for now
-    floatLedgers.value = dummyFloatLedgers;
+  async function fetchBackofficeUsers() {
+    try {
+      const storedUsers = await db.getAll<BackofficeUser>();
+      backofficeUsers.value = storedUsers;
+    } catch (error) {
+      console.error("Error fetching backoffice users from IndexedDB:", error);
+    }
   }
 
-  async function fetchBackofficeUsers(filter: any) {
-    // Simulate API call
-    // You can adjust this based on the filtering criteria or paging
-    backofficeUsers.value = dummyBackofficeUsers;
+  async function fetchTillOperators() {
+    try {
+      const storedOperators = await db.getAll<TillOperator>();
+      tillOperators.value = storedOperators;
+    } catch (error) {
+      console.error("Error fetching till operators from IndexedDB:", error);
+    }
   }
 
-  async function fetchTillOperators(filter: any) {
-    // Simulate API call
-    // You can adjust this based on the filtering criteria or paging
-    tillOperators.value = dummyTillOperators;
+  async function fetchFloatAllocations() {
+    try {
+      const storedAllocations = await db.getAll<FloatAllocation>();
+      floatAllocations.value = storedAllocations;
+    } catch (error) {
+      console.error("Error fetching float allocations from IndexedDB:", error);
+    }
   }
 
-  async function fetchFloatAllocations(filter: any) {
-    // Simulate API call
-    // You can adjust this based on the filtering criteria or paging
-    floatAllocations.value = dummyFloatAllocations;
+  async function fetchFloatRequests() {
+    try {
+      const storedRequests = await db.getAll<FloatRequest>();
+      floatRequests.value = storedRequests;
+    } catch (error) {
+      console.error("Error fetching float requests from IndexedDB:", error);
+    }
   }
 
-  async function fetchFloatRequests(filter: any) {
-    // Simulate API call
-    // You can adjust this based on the filtering criteria or paging
-    floatRequests.value = dummyFloatRequests;
+  // Save individual data items
+  async function saveTransaction(transaction: Transaction) {
+    try {
+      await db.set(transaction.id, transaction);
+      transactions.value.push(transaction);
+    } catch (error) {
+      console.error("Error saving transaction to IndexedDB:", error);
+    }
   }
 
-  // function submit() {
-  //   let payload = {
-  //     amount: form.firstName,
-  //     tillId: form.tillId,
-  //   };
-  //   loading.value = true;
-  //   store
-  //     .allocateFloat(payload)
-  //     .then(() => {
-  //       loading.value = false;
-  //       notify.success(`Float assigned to ${form.tillId}.`);
-  //       emit("cancel");
-  //     })
-  //     .catch(() => {
-  //       loading.value = false;
-  //     });
-  // }
+  async function saveFloatLedger(floatLedger: FloatLedger) {
+    try {
+      await db.set(floatLedger.id, floatLedger);
+      floatLedgers.value.push(floatLedger);
+    } catch (error) {
+      console.error("Error saving float ledger to IndexedDB:", error);
+    }
+  }
 
-  // allocate float function, push to the float allocation array
-  function allocateFloat(payload: AllocateFloat) {
-    floatAllocations.value.push({
+  async function saveFloatRequest(floatRequest: FloatRequest) {
+    try {
+      await db.set(floatRequest.id, floatRequest);
+      floatRequests.value.push(floatRequest);
+    } catch (error) {
+      console.error("Error saving float request to IndexedDB:", error);
+    }
+  }
+
+  async function saveFloatAllocation(floatAllocation: FloatAllocation) {
+    try {
+      await db.set(floatAllocation.id, floatAllocation);
+      floatAllocations.value.push(floatAllocation);
+    } catch (error) {
+      console.error("Error saving float allocation to IndexedDB:", error);
+    }
+  }
+
+  // Approve float request
+  async function approveFloatRequest(requestId: number) {
+    const floatRequest = floatRequests.value.find((request) => request.id === requestId);
+    if (floatRequest) {
+      floatRequest.status = "approved";
+      await db.set(floatRequest.id, floatRequest); // Update in IndexedDB
+    }
+  }
+
+  // Reject float request
+  async function rejectFloatRequest(requestId: number) {
+    const floatRequest = floatRequests.value.find((request) => request.id === requestId);
+    if (floatRequest) {
+      floatRequest.status = "rejected";
+      await db.set(floatRequest.id, floatRequest); // Update in IndexedDB
+    }
+  }
+
+  // Allocate float
+  async function allocateFloat(payload: AllocateFloat) {
+    const floatAllocation: FloatAllocation = {
       id: floatAllocations.value.length + 1,
       dateAssigned: new Date().toISOString(),
       amount: payload.amount,
       status: "Allocated",
       till: payload.tillId,
-    })
-    //save to localstorage
-    // saveTransactionsToLocalStorage();
+    };
+    await saveFloatAllocation(floatAllocation);
   }
 
-  const tillOperatorFloatBalance = ref(0);
-
-  // const saveTransactionsToLocalStorage = () => {
-  //   localStorage.setItem('tillOperatorFloatBalance', JSON.stringify(floatAllocations.value))
-  // }
-
-  // const savedFloatManagerBalance = JSON.parse(localStorage.getItem('tillOperatorFloatBalance') || '0');
-
-  // if (savedFloatManagerBalance) {
-  //   tillOperatorFloatBalance.value = savedFloatManagerBalance;
-  // }
-
-
-  // pass in the requestId
-  // const approveFloatRequest = (requestId: any) => {
-  //   store.approveFloatRequest(requestId);
-  //   store.fetchFloatRequests();
-  //   balanceStore.approveFloatRequest(requestId);
-  //   store.reduceFloatLedger(requestId); 
-  //   store.allocateFloat(requestId);
-  //   console.log(`float request with id ${requestId} approved`);
-  // };
-
-  // allocate float based on approved float request
-  function allocateFloatFromRequest(requestId: any) {
-    const floatRequest = floatRequests.value.find((request) => request.id === requestId);
-
-    if (floatRequest) {
-      floatAllocations.value.push({
-        id: floatAllocations.value.length + 1,
-        dateAssigned: new Date().toISOString(),
-        amount: floatRequest.amount,
-        status: "Allocated",
-        till: floatRequest.till,
-      });
-
-
-      allocateFloatFromRequestToLocalStorage.value.push({
-        id: allocateFloatFromRequestToLocalStorage.value.length + 1,
-        dateAssigned: new Date().toISOString(),
-        status: "Allocated",
-        payload: floatRequest.amount,
-        tillId: floatRequest.till,
-      })
-      saveFloatRequestToLocalStorage();
-    }
-    //save to localstorage
-    // allocateFloatFromRequestToLocalStorage.value.push({
-    //   id: allocateFloatFromRequestToLocalStorage.value.length + 1,
-    //   dateAssigned: new Date().toISOString(),
-    //   status: "Allocated",
-    //   payload: floatRequest.amount,
-    //    status: "Allocated",
-    //    tillId: floatRequest.till,
-    // })
-    // saveFloatRequestToLocalStorage();
-  }
-
-  const allocateFloatFromRequestToLocalStorage = ref<FloatRequest[]>([]);
-
-  const saveFloatRequestToLocalStorage = () => {
-    localStorage.setItem('allocateFloatFromRequestToLocalStorage', JSON.stringify(allocateFloatFromRequestToLocalStorage.value))
-  }
-  
-  // allocate float allocation to float ledger array
-  function adjustFloatLedger(payload: AllocateFloat) {
-    floatLedgers.value.push({
+  // Adjust float ledger
+  async function adjustFloatLedger(payload: AllocateFloat) {
+    const floatLedger: FloatLedger = {
       id: floatLedgers.value.length + 1,
       date: new Date().toISOString(),
-      // description: "Till " + payload.tillId,
       description: payload.tillId,
       amount: -payload.amount,
-      // balance: totalBalance.value + payload.amount,
-    })
+    };
+    await saveFloatLedger(floatLedger);
   }
 
-  //   const allocateFloatFromRequestToLocalStorage = ref<FloatRequest[]>([]);
-
-  // const saveFloatRequestToLocalStorage = () => {
-  //   localStorage.setItem('allocateFloatFromRequestToLocalStorage', JSON.stringify(allocateFloatFromRequestToLocalStorage.value))
-  // }
-
-  // pass in the requestId
-  // const approveFloatRequest = (requestId: any) => {
-  //   store.approveFloatRequest(requestId);
-  //   store.fetchFloatRequests();
-  //   balanceStore.approveFloatRequest(requestId);
-  //   store.reduceFloatLedger(requestId); 
-  //   console.log(`float request with id ${requestId} approved`);
-  // };
-
-  async function reduceFloatLedger(requestId: any) {
-    //  This is local storage 
-
-
-
-
-    // end of local storage
-
-    console.log("Approving float request with ID:", requestId);
-    // Simulate API call
-    // const response = await fetch(`/api/float-requests/${requestId}/approve`, {
-    //   method: "POST",
-    // });
-    // const data = await response.json();
-
-    // use request in floatledgers array id to figure out amount 
-    const floatRequest = floatRequests.value.find(
-      (request) => request.id === requestId
-    );
-    if (!floatRequest) {
-      console.error("Float request not found");
-      return;
-    }
-    floatLedgers.value.push({
-      id: floatLedgers.value.length + 1,
-      date: new Date().toISOString(),
-      description: floatRequest.till,
-      amount: -floatRequest.amount,
-      // balance: 300000000 - floatRequest.amount,
-    });
+  // Initialize data from IndexedDB
+  async function initializeStore() {
+    await fetchTransactions();
+    await fetchFloatLedgers();
+    await fetchBackofficeUsers();
+    await fetchTillOperators();
+    await fetchFloatAllocations();
+    await fetchFloatRequests();
   }
 
-
-  // const rejectFloatRequest = (requestId: any) => {
-  //   store.rejectFloatRequest(requestId);
-  //   store.fetchFloatRequests();
-  //   console.log(`float request with id ${requestId} rejected`);
-  // };
-
-
-
-  // const approveFloatRequest = (requestId: any) => {
-  //   store.approveFloatRequest(requestId);
-  // };
-
-  // const rejectFloatRequest = (requestId: any) => {
-  //   store.rejectFloatRequest(requestId);
-  // };
-
-  // approve float request using passed in Id and set status to approved
-  // function approveFloatRequest(requestId: any) {
-  //   const floatRequest = floatRequests.value.find((request) => request.id === requestId);
-  //   if (floatRequest) {
-  //     floatRequest.status = "Approved";
-  //     floatRequest.approvedBy = "Manager One";
-  //   }
-  // }
-
-  // approve float request using passed in Id and set status to approved and modify the floatrequests array
-  function approveFloatRequest(requestId: any) {
-    console.log("changing status")
-    const floatRequest = floatRequests.value.find((request) => request.id === requestId);
-    if (floatRequest) {
-      floatRequest.status = "approved";
-      // floatRequest.approvedBy = "Manager One";
-    }
-  }
-
-  // reject float request using passed in Id and set status to rejected
-  function rejectFloatRequest(requestId: any) {
-    const floatRequest = floatRequests.value.find((request) => request.id === requestId);
-    if (floatRequest) {
-      floatRequest.status = "rejected";
-    }
-  }
-
+  // Return state and actions
   return {
     transactions,
     totalAmount,
@@ -350,17 +170,20 @@ export const useBilling = defineStore("billing", () => {
     floatAllocations,
     floatRequests,
     tillOperatorFloatBalance,
-    reduceFloatLedger,
-    approveFloatRequest,
-    adjustFloatLedger,
-    rejectFloatRequest,
-    fetchFloatRequests,
     fetchTransactions,
     fetchFloatLedgers,
     fetchBackofficeUsers,
     fetchTillOperators,
     fetchFloatAllocations,
+    fetchFloatRequests,
+    saveTransaction,
+    saveFloatLedger,
+    saveFloatRequest,
+    saveFloatAllocation,
+    approveFloatRequest,
+    rejectFloatRequest,
     allocateFloat,
-    allocateFloatFromRequest,
+    adjustFloatLedger,
+    initializeStore,
   };
 });
