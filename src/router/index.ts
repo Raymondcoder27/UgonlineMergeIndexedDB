08b257495d6971fef41574407734b51d93f7db94
RaymondCoder27 <mwebraymond13@gmail.com>
2025-01-20 16:41:42 +0300

    ignores node modules

--- src/router/index.ts
+++ src/router/index.ts
@@ -210,7 +210,7 @@ router.beforeEach((to, from, next) => {
   if (to.meta.title) {
     document.title = to.meta.title;
   } else {
-    document.title = "UGOnline Portal"; // Fallback title
+    document.title = "UGOnline Admin Portal"; // Fallback title
   }
 
   if (to.meta.requiresAuth) {
